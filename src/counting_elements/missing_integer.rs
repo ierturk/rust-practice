f942b8cf7f41e12d9933864d74fc4f82683e4c60
Ibrahim ERTURK <ierturk@ieee.org>
2023-04-10 15:30:06 +0300

    missing integer

---
+++ src/counting_elements/missing_integer.rs
@@ -0,0 +1,40 @@
+struct MissingInteger;
+
+impl MissingInteger {
+    fn solution(&self, a: &Vec<i32>) -> u32 {
+
+        let mut test_arr = vec![false; 1000_000_001];
+
+        for e in a {
+            if *e > 0 {
+                test_arr[*e as usize] = true;
+            }
+        }
+
+        for (i, c) in test_arr.into_iter().enumerate() {
+            if i>0 && !c {
+                return i as u32;
+            }
+        }
+
+        0
+    }
+
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_max_counters() {
+        let max_counters = MissingInteger;
+
+        assert_eq!( max_counters.solution(vec![1, 3, 6, 4, 1, 2].as_ref() ), 5);
+        assert_eq!( max_counters.solution(vec![1, 2, 3].as_ref() ), 4);
+        assert_eq!( max_counters.solution(vec![-1, -3].as_ref() ), 1);
+
+
+    }
+}
