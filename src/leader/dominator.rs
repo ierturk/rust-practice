cf34a6d6ee30c557e908796ef9c71453717728dd
Ibrahim ERTURK <ierturk@ieee.org>
2023-04-11 17:52:48 +0300

    Leader

--- src/leader/dominator.rs
+++ src/leader/dominator.rs
@@ -1,1 +1,49 @@
+struct Dominator;
 
+impl Dominator {
+    fn solution(a: &Vec<i32>) -> i32 {
+        let mut candidate = -1;
+        let mut size = 0;
+
+        for e in a {
+            if size == 0 {
+                size += 1;
+                candidate = *e;
+            } else {
+                if candidate == *e {
+                    size += 1;
+                } else {
+                    size -= 1;
+                }
+            }
+        }
+
+        if size > 0 {
+            let mut index = -1_i32;
+            let mut count = 0;
+            for i in 0..a.len() {
+                if a[i] == candidate {
+                    count += 1;
+                    if index == -1 {
+                        index = i as i32;
+                    }
+                }
+            }
+
+            if count > a.len() / 2 {
+                return index;
+            }
+        }
+        -1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dominator() {
+        assert_eq!(Dominator::solution(&vec![3, 4, 3, 2, 3, -1, 3, 3]), 0);
+    }
+}
--- src/leader/equi_leader.rs
+++ src/leader/equi_leader.rs
@@ -1,1 +1,72 @@
+use std::collections::VecDeque;
+struct EquiLeader;
 
+impl EquiLeader {
+    fn solution(a: &Vec<i32>) -> i32 {
+        let n = a.len();
+        let mut candidates: VecDeque<i32> = VecDeque::new();
+
+        for i in 0..n {
+            if !candidates.is_empty() {
+                let candidate = *candidates.front().unwrap();
+                if candidate != a[i] {
+                    candidates.pop_front();
+                } else {
+                    candidates.push_front(a[i]);
+                }
+            } else {
+                candidates.push_front(a[i]);
+            }
+        }
+
+        if candidates.is_empty() {
+            return 0;
+        }
+
+        let candidate = *candidates.front().unwrap();
+        let mut count = 0;
+
+        for i in 0..n {
+            if a[i] == candidate {
+                count += 1;
+            }
+        }
+
+        let half: f32 = (n as f32) / 2_f32;
+        if !((count as f32) > half) {
+            return 0;
+        }
+        let leader = candidate;
+        let mut num_equi_leaders = 0;
+        let mut count_x = 0;
+
+        for i in 0..n {
+            if a[i] == leader {
+                count_x += 1;
+            }
+
+            let count_y = count - count_x;
+            let size_x = i + 1;
+            let size_y = n - size_x;
+
+            let half_size_x: f32 = (size_x as f32) / 2_f32;
+            let half_size_y: f32 = (size_y as f32) / 2_f32;
+
+            if (count_x > half_size_x as i32) && (count_y > half_size_y as i32) {
+                num_equi_leaders += 1;
+            }
+        }
+
+        num_equi_leaders
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_equi_leader() {
+        assert_eq!(EquiLeader::solution(&vec![4, 3, 4, 4, 4, 2]), 2);
+    }
+}
