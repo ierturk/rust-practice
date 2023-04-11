4e6f66fdea7519b80f541389356cdd0ac4f6c1b8
Ibrahim ERTURK <ierturk@ieee.org>
2023-04-11 14:36:41 +0300

    count triangles, min abs sum of two

--- src/caterpillar_method/count_triangles.rs
+++ src/caterpillar_method/count_triangles.rs
@@ -1,1 +1,32 @@
+struct CountTriangles;
 
+impl CountTriangles {
+    fn solution(a: &mut Vec<u32>) -> usize {
+        let n = a.len();
+        let mut triangles = 0;
+
+        a.sort();
+
+        for p in 0..(n - 2) {
+            let mut r = p + 2;
+
+            for q in (p + 1)..(n - 1) {
+                while (r < n) && (a[p] > (a[r] - a[q])) {
+                    r += 1;
+                }
+                triangles += r - q - 1;
+            }
+        }
+        triangles
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_count_triangles() {
+        assert_eq!(CountTriangles::solution(&mut vec![10, 2, 5, 1, 8, 12]), 4);
+    }
+}
--- src/caterpillar_method/min_abs_sum_of_two.rs
+++ src/caterpillar_method/min_abs_sum_of_two.rs
@@ -1,1 +1,47 @@
+use std::cmp::min;
 
+struct MinAbsSumOfTwo;
+
+impl MinAbsSumOfTwo {
+    fn solution(a: &mut Vec<i32>) -> usize {
+        if a.len() == 1 {
+            return (a[0] + a[0]).abs() as usize;
+        }
+
+        a.sort();
+
+        let total_min = a.iter().min();
+        if total_min >= Some(&0) {
+            return 2 * a[0] as usize;
+        }
+
+        let mut minimal_sum = i32::MAX;
+
+        let mut p = 0;
+        let mut q = a.len() - 1;
+        while p < q {
+            minimal_sum = min(minimal_sum, (a[p] + a[q]).abs());
+            minimal_sum = min(minimal_sum, (a[p] + a[p]).abs());
+            minimal_sum = min(minimal_sum, (a[q] + a[q]).abs());
+
+            if (a[p + 1] + a[q]).abs() <= (a[p] + a[q - 1]).abs() {
+                p += 1;
+            } else {
+                q -= 1;
+            }
+        }
+
+        min(minimal_sum, (a[p] + a[q]).abs()) as usize
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_min_abs_sum_of_two() {
+        assert_eq!(MinAbsSumOfTwo::solution(&mut vec![1, 4, -3]), 1);
+        assert_eq!(MinAbsSumOfTwo::solution(&mut vec![-8, 4, 5, -10, 3]), 3);
+    }
+}
