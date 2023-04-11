4cfdfd0e641377c982c5ab28757d486bde01e98e
Ibrahim ERTURK <ierturk@ieee.org>
2023-04-11 17:06:40 +0300

    Nesting, StoneWall

--- src/stacks_and_queues/nesting.rs
+++ src/stacks_and_queues/nesting.rs
@@ -1,1 +1,42 @@
+struct Nesting;
 
+impl Nesting {
+    fn solution(s: &str) -> i32 {
+        if s.is_empty() {
+            return 1;
+        }
+
+        if s.len() % 2 != 0 {
+            return 0;
+        }
+
+        let mut open = 0;
+        for c in s.chars() {
+            if c == '(' {
+                open += 1;
+            } else {
+                if open > 0 {
+                    open -= 1;
+                } else {
+                    return 0;
+                }
+            }
+        }
+
+        if open == 0 {
+            return 1;
+        }
+        0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_nesting() {
+        assert_eq!(Nesting::solution("(()(())())"), 1);
+        assert_eq!(Nesting::solution("())"), 0);
+    }
+}
--- src/stacks_and_queues/stone_wall.rs
+++ src/stacks_and_queues/stone_wall.rs
@@ -1,1 +1,33 @@
+use std::collections::VecDeque;
 
+struct StoneWall;
+
+impl StoneWall {
+    fn solution(h: &Vec<i32>) -> i32 {
+        let mut stack: VecDeque<i32> = VecDeque::new();
+        let mut stones = 0;
+
+        for height in h {
+            while !stack.is_empty() && stack.front().unwrap() > height {
+                stack.pop_front();
+            }
+            if !stack.is_empty() && stack.front().unwrap() == height {
+                continue;
+            }
+
+            stones += 1;
+            stack.push_front(*height);
+        }
+        stones
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_stone_wall() {
+        assert_eq!(StoneWall::solution(&vec![8, 8, 5, 7, 9, 8, 7, 4, 8]), 7);
+    }
+}
