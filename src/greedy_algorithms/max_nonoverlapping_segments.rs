0804d00d2e029f9bdd7db727eac275f0d6bbbde3
Ibrahim ERTURK <ierturk@ieee.org>
2023-04-11 11:05:59 +0300

    greedy algorithms

--- src/greedy_algorithms/max_nonoverlapping_segments.rs
+++ src/greedy_algorithms/max_nonoverlapping_segments.rs
@@ -1,1 +1,40 @@
+struct MaxNonoverlappingSegments;
 
+impl MaxNonoverlappingSegments {
+    fn solution(a: &mut Vec<i32>, b: &mut Vec<i32>) -> u32 {
+        let a_len = a.len();
+        let b_len = b.len();
+
+        if a_len == 0 || a_len != b_len {
+            return 0;
+        }
+
+        let mut count = 1;
+        let mut prev_end = b[0];
+
+        for i in 1..a_len {
+            if a[i] > prev_end {
+                count += 1;
+                prev_end = b[i];
+            }
+        }
+
+        return count;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_max_nonoverlapping_segments() {
+        assert_eq!(
+            MaxNonoverlappingSegments::solution(
+                &mut vec![1, 3, 7, 9, 9],
+                &mut vec![5, 6, 8, 9, 10]
+            ),
+            3
+        );
+    }
+}
--- src/greedy_algorithms/tie_ropes.rs
+++ src/greedy_algorithms/tie_ropes.rs
@@ -1,1 +1,27 @@
+struct TieRopes;
 
+impl TieRopes {
+    fn solution(k: u32, a: &mut Vec<u32>) -> u32 {
+        let mut count = 0;
+        let mut rop_sum = 0;
+
+        for e in a {
+            rop_sum += *e;
+            if rop_sum >= k {
+                count += 1;
+                rop_sum = 0;
+            }
+        }
+        count
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_tie_ropes() {
+        assert_eq!(TieRopes::solution(4, &mut vec![1, 2, 3, 4, 1, 1, 3]), 3);
+    }
+}
