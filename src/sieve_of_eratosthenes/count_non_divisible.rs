956dba589b3406f9a7d5b04c512c37cfaf9b26de
Ibrahim ERTURK <ierturk@ieee.org>
2023-04-11 20:46:23 +0300

    Sieve Of Eratosthenes

--- src/sieve_of_eratosthenes/count_non_divisible.rs
+++ src/sieve_of_eratosthenes/count_non_divisible.rs
@@ -1,1 +1,49 @@
+struct CountNonDivisible;
 
+impl CountNonDivisible {
+    fn solution(a: &Vec<i32>) -> Vec<i32> {
+        let n = a.len();
+        let max_element = *a.iter().max().unwrap();
+        let mut counts = vec![0_i32; max_element as usize + 1];
+
+        for i in a {
+            counts[*i as usize] += 1;
+        }
+
+        let mut answer = vec![0_i32; n];
+
+        for i in 0..n {
+            let mut divisors = 0_i32;
+            let num = a[i];
+
+            let mut j = 1_i32;
+            while j * j <= num {
+                if num % j == 0 {
+                    divisors += counts[j as usize];
+                    if num / j != j {
+                        divisors += counts[(num / j) as usize];
+                    }
+                }
+
+                j += 1;
+            }
+
+            answer[i] = n as i32 - divisors;
+        }
+
+        answer
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_count_non_divisible() {
+        assert_eq!(
+            CountNonDivisible::solution(&vec![3, 1, 2, 3, 6]),
+            vec![2, 4, 3, 2, 0]
+        );
+    }
+}
--- src/sieve_of_eratosthenes/count_semiprimes.rs
+++ src/sieve_of_eratosthenes/count_semiprimes.rs
@@ -1,1 +1,58 @@
+struct CountSemiprimes;
 
+impl CountSemiprimes {
+    fn get_fact_arr(n: usize) -> Vec<i32> {
+        let mut f = vec![0_i32; n + 1];
+
+        f[1] = 1;
+        let mut i = 2;
+
+        while i * i <= n {
+            if f[i] == 0 {
+                let mut k = i * i;
+                while k <= n {
+                    if f[k] == 0 {
+                        f[k] = i as i32;
+                    }
+                    k += i;
+                }
+            }
+            i += 1;
+        }
+        f
+    }
+
+    fn solution(n: usize, p: &Vec<i32>, q: &Vec<i32>) -> Vec<i32> {
+        let f = CountSemiprimes::get_fact_arr(n);
+        let mut prefix_semi_primes = vec![0_i32; n + 1];
+
+        for x in 1..=n {
+            if f[x] > 0 && f[x / f[x] as usize] == 0 {
+                prefix_semi_primes[x] += 1;
+            }
+            prefix_semi_primes[x] += prefix_semi_primes[x - 1];
+        }
+
+        let m = p.len();
+        let mut ans = vec![0_i32; m];
+
+        for i in 0..m {
+            ans[i] = prefix_semi_primes[q[i] as usize] - prefix_semi_primes[p[i] as usize - 1];
+        }
+
+        ans
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_count_semiprimes() {
+        assert_eq!(
+            CountSemiprimes::solution(26, &vec![1, 4, 16], &vec![26, 10, 20]),
+            vec![10, 4, 0]
+        );
+    }
+}
